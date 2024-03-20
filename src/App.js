df1f55136951e4b5e7bc7615319248bf9cb5d1f1
Dan Dullis <daniel.gullis1@gmail.com>
2024-03-20 14:28:55 +0000

    responseive image sizeing

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ function App() {
     <Container fluid className="p-3 m-3 rounded-2" style={{maxWidth: '700px', overflowWrap: 'break-word'}}>
       <Row className="justify-content-center">
       <Row className="mb-3 justify-content-center">
-          <Row className="justify-content-center" style={{fontFamily: 'Quicksand, sans-serif', fontSize: 'calc(20px + 4vw)', letterSpacing: '2px', color: 'white', fontWeight: '800'}}>
+          <Row className="justify-content-center" style={{fontFamily: 'Quicksand, sans-serif', fontSize: 'calc(20px + 3vw)', letterSpacing: '2px', color: 'white', fontWeight: '800'}}>
           Dan Gullis
           </Row>
           <Row className="justify-content-center mb-2"  style={{fontFamily: 'Quicksand, sans-serif', fontSize: 'calc(10px + 2vw)', letterSpacing: '2px', color: 'white', fontWeight: '600', textAlign: 'center'}}>
@@ -52,8 +52,8 @@ function App() {
                     alt='github-logo' 
                     src='/github-mark-white.png' 
                     style={{
-                      width: 'min(30px, 7vw)',  
-                      height: 'min(30px, 7vw)', 
+                      width: 'min(30px, 5vw)',  
+                      height: 'min(30px, 5vw)', 
                     }}/>
               </div>
             </a>
@@ -65,8 +65,8 @@ function App() {
                     alt='linekdin-logo' 
                     src='In-White-128@2x.png' 
                     style={{
-                      width: 'min(30px, 7vw)',  
-                      height: 'min(30px, 7vw)', 
+                      width: 'min(30px, 5vw)',  
+                      height: 'min(30px, 5vw)', 
                     }}/>
               </div>
             </a>
@@ -78,8 +78,8 @@ function App() {
                     alt='email-logo' 
                     src='email-icon.png' 
                     style={{
-                      width: 'min(40px, 9vw)',  
-                      height: 'min(30px, 7vw)', 
+                      width: 'min(40px, 7vw)',  
+                      height: 'min(30px, 5vw)', 
                     }}/>
               </div>
             </a>
@@ -103,30 +103,34 @@ function App() {
       :
       (
           <>
-          <Col className="d-flex flex-column justify-content-center p-4" >
+          <Col className="d-flex flex-column justify-content-center p-2">
+            <Row style={{minWidth: '300px'}}>
+                <img 
+                  alt='profile' 
+                  src='profile-image3.jpg'
+                  className='image-fluid profile-picture' 
+                  />
+            </Row>
+          </Col>
+
+          <Col className="d-flex flex-column justify-content-center p-3" >
             <Row>
-          <Button onClick={() => openInfo("ABOUT")} className="bg-gradient p-3 mb-4 mt-4 rounded-1" style={{backgroundColor: '#f5a65b', border: 'none', fontSize: '20px'}}>
+          <Button onClick={() => openInfo("ABOUT")} className="bg-gradient p-sm-2 p-md-3 p-lg-3.5 mb-4 mt-4 rounded-1" style={{backgroundColor: '#f5a65b', border: 'none', fontSize: '20px'}}>
             About Me
           </Button>
           </Row>
           <Row>
-          <Button onClick={() => openInfo("PROJECTS")} className="bg-gradient p-3 mb-4 rounded-1" style={{backgroundColor: '#f5a65b', border: 'none', fontSize: '20px'}}>
+          <Button onClick={() => openInfo("PROJECTS")} className="bg-gradient p-sm-2 p-md-3 p-lg-3.5  mb-4 rounded-1" style={{backgroundColor: '#f5a65b', border: 'none', fontSize: '20px'}}>
             Projects
           </Button>
           </Row>
           <Row>
-          <Button onClick={() => openInfo("EXPERIENCE")} className="bg-gradient p-3 mb-4 rounded-1" style={{backgroundColor: '#f5a65b', border: 'none', fontSize: '20px'}}>
+          <Button onClick={() => openInfo("EXPERIENCE")} className="bg-gradient p-sm-2 p-md-3 p-lg-3.5  mb-4 rounded-1" style={{backgroundColor: '#f5a65b', border: 'none', fontSize: '20px'}}>
             Experience
           </Button>
           </Row>      
           </Col>
-          <Col>
-            <Row>
-              <div className="placeholder">
-                <img alt='profile' src='profile-image2.jpg' style={{ height: '400px'}} />
-              </div>
-            </Row>
-          </Col>
+          
         
         </>
 
